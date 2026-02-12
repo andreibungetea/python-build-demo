089f8702d59a4ba5fa020071de0f48fc7952a16c
Bungetea Andrei <andreibungetea@gmail.com>
2026-02-12 11:30:17 +0200

    redu failure test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,3 @@ from utils import calculate_total
 def test_calculate_total():
     assert calculate_total([10, 15, 25]) == 50
     assert calculate_total([]) == 0
-
-def test_calculate_total():
-    assert calculate_total([10, 15, 25]) == 51
-    assert calculate_total([]) == 0
